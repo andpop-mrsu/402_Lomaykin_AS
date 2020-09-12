40a0a912734fe95f0808d9e25470545a4d08a733
Yosha-exe <alexmarkeloff5@gmail.com>
2020-09-12 11:10:31 +0300

    test_commit

---
+++ Task01/self-logger.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+chcp 65001>nul
+
+echo.
+echo CREATE TABLE IF NOT EXISTS users (name TEXT NOT NULL, date TEXT NOT NULL ); | sqlite3 users.db
+echo insert into users values('%USERNAME%', datetime('now', 'localtime')); | sqlite3 users.db
+
+echo Имя программы: %~nx0
+echo|<nul set /p="Количество запусков: "
+echo select count(*) from users; | sqlite3 users.db
+echo|<nul set /p="Первый запуск: "
+echo select Date from users order by Date asc limit 1; | sqlite3 users.db
+
+echo.
+echo select * from users; | sqlite3 -table users.db
+echo.
+
+pause
