4f1e9ebc6ecae0968d0debba36a885aa85e35d13
Yosha-exe <alexmarkeloff5@gmail.com>
2020-09-20 11:35:34 +0300

    gitignore_fix

--- Task02/hangman/src/Controller.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Yoshaexe\hangman\Controller;
-    use function Yoshaexe\hangman\View\showGame;
-
-    function startGame() {
-        echo "The game started".PHP_EOL;
-        showGame();
-    }
-?>
--- Task02/hangman/src/View.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Yoshaexe\hangman\View;
-
-    function showGame() {
-        echo "The game displayed";
-    }
-?>
