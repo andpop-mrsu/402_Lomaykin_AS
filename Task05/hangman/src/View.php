668cce30edd3c78d35e8260c90608d9e092566d2
Yosha-exe <alexmarkeloff5@gmail.com>
2020-11-07 13:45:13 +0300

    added Task05 solution

---
+++ Task05/hangman/src/View.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Yoshaexe\hangman\View;
+
+use function cli\line;
+
+function showGame($fails, $gameField)
+{
+    $graphic = array(
+        " +---+\n     |\n     |\n     |\n    ===\n ",
+        " +---+\n 0   |\n     |\n     |\n    ===\n ",
+        " +---+\n 0   |\n |   |\n     |\n    ===\n ",
+        " +---+\n 0   |\n/|   |\n     |\n    ===\n ",
+        " +---+\n 0   |\n/|\  |\n     |\n    ===\n ",
+        " +---+\n 0   |\n/|\  |\n/    |\n    ===\n ",
+        " +---+\n 0   |\n/|\  |\n/ \  |\n    ===\n "
+    );
+
+    echo "\n";
+
+    line($graphic[$fails]);
+    line($gameField);
+
+    echo "\n";
+}
