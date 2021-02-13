e88c67a65ab69b1cd34c3850066a9e8a30e43afe
Yosha-exe <alexmarkeloff5@gmail.com>
2021-02-13 23:35:12 +0300

    upd Task02

---
+++ Task02_2/tests/BookTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Book;
+
+class BookTest extends TestCase
+{
+    public function testSetName()
+    {
+        $book = new Book();
+        $book->setTittle("PHP 7 в подлиннике");
+
+        self::assertEquals("PHP 7 в подлиннике", $book->getTittle());
+    }
+
+    public function testSetAuthors()
+    {
+        $book = new Book();
+        $book->setAuthors(array("Дмитрий Котеров", "Игорь Симдянов"));
+
+        self::assertEquals(array("Дмитрий Котеров", "Игорь Симдянов"), $book->getAuthors());
+    }
+
+    public function testSetPublisher()
+    {
+        $book = new Book();
+        $book->setPublisher("БХВ-Петербург");
+
+        self::assertEquals("БХВ-Петербург", $book->getPublisher());
+    }
+
+    public function testSetYear()
+    {
+        $book = new Book();
+        $book->setYear(20216);
+
+        self::assertEquals(2016, $book->getYear());
+    }
+}
